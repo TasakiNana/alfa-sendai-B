97c0844067db2eaf04ffc32383cdcfd65388c52f
TasakiNana <89884165+TasakiNana@users.noreply.github.com>
2021-09-13 15:27:53 +0900

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,7 +21,7 @@ const app = Vue.createApp({
           q1:'植木',
         },
         stage2: {
-          q1: 'いいい',
+          q1: 'rabbit',
           // q2: 'えええ',
           // q3: 'おおお'
         },
